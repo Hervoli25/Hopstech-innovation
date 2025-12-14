5f03053f9d7c45b038340223cf895bd91d8cdb07
Hervoli25 <herve@hopstech.com>
2025-12-14 19:08:31 +0100

    fix(sw): update revision number for index.html in service worker

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-21a80088'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.pcg42i7s13s"
+    "revision": "0.gqc7hnsuqeo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
